b6e5e4e9cac9eb07883a40c17cd82cad3b4fd5e3
Omar202204 <omarpf2004@gmail.com>
2026-02-17 08:34:10 -0600

    Avances dia 2 semana 2

---
+++ semana2/S2Dia2_Promesas.js
@@ -0,0 +1,13 @@
+// En Javascript, una Promise es un Objeto que representa la eventual finalización 
+// (o falla) de una operación asíncrona.
+
+// Es como un "Ticket de cambio" o un "Pagaré".
+
+// Tiene 3 estados posibles:
+
+// Pending (Pendiente): El proceso está corriendo (la pizza se está horneando).
+
+// Resolved / Fulfilled (Resuelta): Todo salió bien (te entregaron la pizza).
+
+// Rejected (Rechazada): Algo falló (se quemó la pizza o no hay ingredientes).
+
