7c024a43c9c259ef616733c241b120a8576582f3
Omar202204 <omarpf2004@gmail.com>
2026-03-02 11:09:02 -0600

    Practica de conceptos

--- semana1/Dia2_ArrayMethods.js
+++ semana1/Dia2_ArrayMethods.js
@@ -5,13 +5,7 @@
 // 3- .find(): Busca un solo elemento (el primero que coincida) y te lo entrega. (Ej: Encontrar al usuario con ID 55).
 
 
-
-// "El Inventario de Hardware" 
-
-// Instrucciones: Crea un archivo Dia2.js y escribe este código (no copies/pegues, 
-// teclealo para memorizar la sintaxis):
-
-// Declarar la data: Crea un array de objetos con productos. Usa const.
+// Array de objetos con productos
 
 const listaSuper = [
     {id : 1, nombre : "Leche", marca : "19 hermanos", precio : 50},
@@ -31,7 +25,7 @@ console.log(preciosCaros);
 let nombres = listaSuper.map(producto => producto.nombre)
 console.table(nombres);
 
-// Buscar (Find): Usa .find() para encontrar el objeto que tenga el id: 3.
+// Buscar (Find)
 let id1 = listaSuper.find(producto => {
     return producto.id === 1})
 
@@ -40,3 +34,31 @@ console.log(id1.nombre);
 const nombre = id1 ? id1.nombre : undefined;
 
 console.log(nombre);
+
+
+const numeros = [
+    1, 2, 3, 4, 5
+]
+
+
+const pares = numeros.filter(n => n%2==0)
+console.log(pares);
+
+const triples = numeros.map(n => n*3)
+console.log(triples); 
+
+const suma = numeros.reduce((acumulador, valorActual) =>
+    acumulador + valorActual, 0)    // definimos que empieza desde cero
+
+console.log(suma);
+
+let sumaFor= 0;
+
+for (let index = 0; index < numeros.length; index++) {
+    sumaFor += numeros[index]
+    console.log(`SumaFor va en: ${sumaFor}`);   
+}
+
+console.log(numeros.length);
+
+console.log(sumaFor);
--- semana1/Dia3_OperadoresObjetos.js
+++ semana1/Dia3_OperadoresObjetos.js
@@ -1,21 +1,38 @@
-// Crea un objeto llamado configDefault con: // { ip: "192.168.1.1", puerto: 80, admin: "root" }.
 const configDefault = {
     ip : "192.168.1.1",
     puerto : 80,
     admin : "root"
 }
-// Crea un objeto usuarioConfig con: { admin: "Omar", tema: "Oscuro" }.
 const usuarioConfig = {
     admin : "Omar",
     tema : "Oscuro"
 }
-// Usa el Spread Operator (...) para crear una configFinal que combine ambos. 
+// Spread Operator (...)  que combina ambos. 
 const configFinal = {...configDefault, ...usuarioConfig}
 console.log(configFinal);
-// (Ojo: El admin debe quedar como "Omar", así que pon el usuario después del default).
+// (Ojo: Si se repiten las propiedadades del objeto gana la que quedo al final en el Spread Operator).
 
 // Usa Destructuring para sacar las variables ip y admin de esa configFinal.
 const {ip, admin} = configFinal
 console.log(Number(ip));
 // Imprime: `Conectando a ${ip} como usuario: ${admin}`.
 console.log(`Conectando a ${ip} como usuario: ${admin}`);
+
+const persona = {
+    nombre: "Omar",
+    edad: 21,
+    ciudad: "Tepa"
+}
+
+console.log("");
+
+// Para cambiar copiaPersona sin modificar el persona: 
+const copiaPersona = {...persona, ciudad: "Guadalajara"}
+
+// Destructuring de objetos
+const{nombre, edad, ciudad} = persona
+
+
+console.log(nombre, ciudad);
+
+console.log(copiaPersona);
